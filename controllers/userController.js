2a385f336ecbb3f01cd498aa8eb7cfb08f78a9ff
alfirmS <130156102+alfirmS@users.noreply.github.com>
2025-04-11 16:53:10 +0700

    Add controllers/userController.js 1744365188980

---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const users = []; // In-memory storage for users
+
+exports.registerUser = async (req, res) => {
+    const { username, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = { username, password: hashedPassword, role };
+    users.push(newUser);
+    res.status(201).json(newUser);
+};
+
+exports.loginUser = async (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+    if (!user) return res.sendStatus(404);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.sendStatus(403);
+
+    const token = jwt.sign({ username: user.username, role: user.role }, 'your_secret_key');
+    res.json({ token });
+};
+
+// Additional user authentication methods can be added here
