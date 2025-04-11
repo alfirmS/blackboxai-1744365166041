9edac35939fef7bfd2e9ca37cd1328cf041dd770
alfirmS <130156102+alfirmS@users.noreply.github.com>
2025-04-11 17:13:59 +0700

    Add middlewares/errorHandler.js 1744366437931

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,7 @@
 exports.errorHandler = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ message: 'An unexpected error occurred!' });
+    res.status(err.status || 500).json({
+        message: err.message || 'An unexpected error occurred!',
+        errors: err.errors || []
+    });
 };
